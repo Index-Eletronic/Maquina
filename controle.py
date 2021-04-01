febbbcb62cbcca5df9caa4e92027eaffbdd284da
Fil Tuon <f.tuon887@gmail.com>
2021-04-01 10:13:59 -0300

    Initial commit

---
+++ controle.py
@@ -0,0 +1,11 @@
+from PyQt5 import uic,QtWidgets
+
+def funcao_principal():
+    print('teste')
+
+app=QtWidgets.QApplication([])
+interface=uic.loadUi("interface.ui")
+interface.BtnCortar.clicked.connect(funcao_principal)
+
+interface.show()
+app.exec()
