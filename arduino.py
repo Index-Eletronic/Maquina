823bf9d004258a3f94af32924fcf6a89361e6794
Fil Tuon <f.tuon887@gmail.com>
2021-04-02 09:57:27 -0300

    .

--- arduino.py
+++ arduino.py
@@ -3,28 +3,32 @@ from PyQt5 import uic,QtWidgets
 import time
 
 #========== Conexão com Arduino ================================
-Uno = Arduino('COM13') # Portas que está conectado o arduino
+Uno = Arduino('COM1') # Portas que está conectado o arduino
 it = util.Iterator(Uno)
 it.start()
 #===========Configuração dos Pinos==============================
-valD13 = Uno.get_pin('d:13:i')# Pino 13 é Input
+valD13 = Uno.get_pin('d:13:i')# Pino 13 é Input controladas por botão/sensor (d = digital, numero da porta, input  = pinMode
 valD12 = Uno.get_pin('d:12:i')# Pino 12 é Input
-valD11 = Uno.get_pin('d:11:i')# Pino 11 é Input
+valD11 = ('interface.ui[BtnCortar]')# Pino 11 é Input
 #===========Configuração dos Pinos==============================
-estadoBt1Ant = True #Estado HIGH - Ligado
-estadoBt2Ant = True #Estado HIGH - Ligado
-estadoBt3Ant = True #Estado HIGH - Ligado
+#estadoBt1Ant = True #Variavel que detecta o estado do botão
+#estadoBt2Ant = True #Variavel que detecta o estado do botão
+#estadoBt3Ant = True #Variavel que detecta o estado do botão
 #========== Conexão com Interface================================
+
+while True: # = Void Loop()
+    time.sleep(0.15)
+    def funcao_principal():
+        if valD11 == True:
+            Uno.digital[13].write(1)
+            print('LED ARDUINO ACENDEU')
+        else:
+            Uno.digital[13].write(0)
+    break
+
 app=QtWidgets.QApplication([])
 interface=uic.loadUi("interface.ui")
-BtnCortar = interface.BtnCortar()
-BtnParar = interface.BtnParar()
-def cortar():
-    Uno.digital[5].write(1)
-    while True:
-        time.sleep(0.15)
-        btnCortar  = valD11.read()
-        btnParar = valD12.read()
-        btnCortar = BtnCortar
-        if btnCortar == BtnCortar:
-            interface.BtnCortar.clicked.connect(cortar)
+interface.BtnCortar.clicked.connect(funcao_principal)
+
+interface.show()
+app.exec()
